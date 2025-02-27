81e9b9ffee0e8f78f650f8b2a3d7c1f6faeef0bb
Giacomo Giorgi <giacomo1215@gmail.com>
2025-02-27 20:11:03 +0100

    Initial commit

---
+++ Color.java
@@ -0,0 +1,23 @@
+public class Color {
+    private final int r, g, b;
+
+    public Color(int r, int g, int b) {
+        this.r = Math.max(0, Math.min(255, r));
+        this.g = Math.max(0, Math.min(255, g));
+        this.b = Math.max(0, Math.min(255, b));
+    }
+
+    public int getR() { return r; }
+    public int getG() { return g; }
+    public int getB() { return b; }
+
+    String getHex() { return String.format("#%02X%02X%02X", r, g, b); }
+    int getDecimal() { return (r << 16) | (g << 8) | b; }
+
+    public double[] toYUV() {
+        double y = 0.299 * r + 0.587 * g + 0.114 * b;
+        double u = 0.493 * (b - y);
+        double v = 0.877 * (r - y);
+        return new double[]{y, u, v};
+    }
+}
---
+++ Program.java
@@ -0,0 +1,10 @@
+public class Program {
+    public static void main(String[] args) {
+
+        int width = 120;
+        int height = 120;
+
+        Color[][] image = new Color[width][height];
+
+    }
+}
